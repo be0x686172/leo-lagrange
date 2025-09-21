9c99ae8996a540b7ae2a49da2100810c2b38b835
be.0x686172 <abderahmane.beharrahala@gmail.com>
2025-09-21 10:23:05 +0200

    -

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -1,9 +1,75 @@
 import './style.scss';
+import { Box } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 
-const Grid = () => {
+const Grid = (props) => {
+
+    function handleEdit(id)
+    {
+        console.log(id);
+    }
+
+    const columns = [
+        {
+            field: 'name',
+            headerName: 'Nom',
+            flex: 1,
+            editable: false,
+        },
+        {
+            field: 'firstname',
+            headerName: 'Prénom',
+            flex: 1,
+            editable: false,
+        },
+        {
+            field: 'email',
+            headerName: 'Adresse électronique',
+            flex: 1.3,
+            editable: false,
+        },
+        {
+            field: 'role',
+            headerName: 'Rôle',
+            flex: 1,
+            editable: false,
+        },
+        {
+            field: 'candidates_access',
+            headerName: 'Accès candidats',
+            flex: 1,
+            editable: false,
+        },
+        {
+            field: 'interviews_access',
+            headerName: 'Accès entretiens',
+            flex: 1,
+            editable: false,
+        },
+        {
+            field: 'action',
+            headerName: 'Action',
+            flex: 0.7,
+            editable: false,
+            renderCell: (params) => (
+                <img
+                    src="icons/square_pen.png"
+                    alt="Edit"
+                    style={{ width: 17, height: 17, cursor: 'pointer' }}
+                    onClick={() => handleEdit(params.row.id)}
+                />
+            )
+        }
+    ];
+
     return (
         <div className='component c-Grid'>
-            Grid
+            <Box sx={{ width: '100%' }}>
+                <DataGrid
+                    rows={props.profiles}
+                    columns={columns}
+                />
+            </Box>
         </div>
     );
 };
--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,34 +1,26 @@
 import './style.scss';
 import { supabase } from '../../services/supabaseClient';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Header from '../../components/header/Header';
 import Grid from '../../components/grid/Grid';
 
 const Users = () => {
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            // Récupérer la session actuelle
-            const { data: { session } } = await supabase.auth.getSession();
-            const accessToken = session?.access_token;
+    const [profiles, setProfiles] = useState(null);
 
-            // Fetch vers ta fonction Edge
-            const res = await fetch('https://gjpaydokzdgjyvtpvwku.supabase.co/functions/v1/listAllUsers', {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            });
-            const data = await res.json();
-            console.log(data);
+    useEffect(() => {
+        const fetchProfiles = async () => {
+            const {data: profiles, error} = await supabase.from('profiles').select('*');
+            setProfiles(profiles);
         };
 
-        fetchUsers();
+        fetchProfiles();
     }, []);
 
     return (
         <div className='page p-Users'>
             <Header />
-            Grid
+            <Grid profiles={profiles} />
         </div>
     );
 };
