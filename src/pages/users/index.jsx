04e42f7e836ef751a0736998939e14efefb34e89
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-10-29 20:20:31 +0100

    save

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -33,7 +33,7 @@ const UsersPage = () => {
 
     return (
         <div className='page users-page'>
-            <TableContainerFeature version={"users"}   columns={["Nom", "Prénom", "E-mail", "Statut", "Accès candidats", "Accès entretiens", "Action"]} data={users} lengthData={Object.keys(usersData).length} />
+            <TableContainerFeature version={"users"}   columns={["E-mail", "Nom", "Prénom", "Statut", "Accès candidats", "Accès entretiens", "Action"]} data={users} lengthData={Object.keys(usersData).length} />
         </div>
     );
 };
