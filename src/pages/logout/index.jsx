bc1a10454adf28489d91fb85514f70c3024cd796
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-10-30 18:01:42 +0100

    save

--- src/pages/logout/index.jsx
+++ src/pages/logout/index.jsx
@@ -8,9 +8,8 @@ const LogoutPage = () => {
 
     useEffect(() => {
         supabaseSignOut()
-        .then(() => {
-            navigate('/login');
-        });
+        .then(() => { navigate('/login'); })
+        .catch(() => { navigate('/login'); });
     })
 
     return (
