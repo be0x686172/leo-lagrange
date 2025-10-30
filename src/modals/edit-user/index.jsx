fe7d96a1a7c31072342fe095502e1d7c350bf82a
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-10-30 12:23:18 +0100

    save

--- src/modals/edit-user/index.jsx
+++ src/modals/edit-user/index.jsx
@@ -3,13 +3,29 @@ import { X } from 'lucide-react';
 import TextInputUI from '../../components/ui/text-input';
 import SelectUI from '../../components/ui/select';
 import SwitchUI from '../../components/ui/switch';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ButtonUI from '../../components/ui/button';
+import { supabaseGetUserById } from '../../services/supabase/supabaseDatabase';
+import { data } from 'react-router';
 
 const EditUserModal = ({userId, setOpenEditUserModal}) => {
     
+    const [user, setUser] = useState(null);
     const [candidatesAccess, setCandidatesAccess] = useState(false);
     const [interviewsAccess, setInterviewsAccess] = useState(false);
+
+    useEffect(() => {
+        supabaseGetUserById(userId).then((data) => {
+            setUser({
+                ...data[0]
+            });
+        })
+    }, [userId]);
+
+    function handleForm(event) {
+        event.preventDefault();
+        console.log(user);
+    }
     
     return (
         <div className='edit-user-modal'>
@@ -18,22 +34,26 @@ const EditUserModal = ({userId, setOpenEditUserModal}) => {
                 <h1>Modifier un utilisateur</h1>
                 <form>
                     <div>
-                        <TextInputUI label="Nom" name={"name"} type={"text"} />
-                        <TextInputUI label="Prénom" name={"firstname"} type={"text"} />
+                        <TextInputUI label="Nom" name={"name"} type={"text"} action={setUser} />
+                        <TextInputUI label="Prénom" name={"firstname"} type={"text"} action={setUser} />
                     </div>
-                    <TextInputUI label="E-mail" name={"email"} type={"email"} />
-                    <TextInputUI label="Mot de passe" name={"password"} type={"password"} />
-                    <SelectUI label={"Statut"} name={"role"} />
+                    <TextInputUI label="E-mail" name={"email"} type={"email"} action={setUser}/>
+                    <TextInputUI label="Mot de passe" name={"password"} type={"password"} action={setUser} />
+                    <SelectUI label={"Statut"} name={"role"} action={setUser} />
+                    {user ? (
+                        <div>
+                            <label>Accès candidats</label>
+                            <SwitchUI id={"candidates_access"} isOn={user.candidates_access} handleToggle={() => setUser(prev => ({ ...prev, candidates_access: !prev.candidates_access })) }/>
+                        </div>
+                    ) : ''}
+                    {user ? (
+                        <div>
+                            <label>Accès entretiens</label>
+                            <SwitchUI id={"interviews_access"} isOn={user.interviews_access} handleToggle={() => setUser(prev => ({ ...prev, interviews_access: !prev.interviews_access }))}/>
+                        </div>
+                    ) : ''}
                     <div>
-                        <label>Accès candidats</label>
-                        <SwitchUI id={"candidates_access"} isOn={candidatesAccess} handleToggle={() => setCandidatesAccess(!candidatesAccess)}/>
-                    </div>
-                    <div>
-                        <label>Accès entretiens</label>
-                        <SwitchUI id={"interviews_access"} isOn={interviewsAccess} handleToggle={() => setInterviewsAccess(!interviewsAccess)}/>
-                    </div>
-                    <div>
-                        <ButtonUI text={"Confirmer"} className={"button-primary"} />
+                        <ButtonUI text={"Confirmer"} className={"button-primary"} action={(event) => handleForm(event)}/>
                         <ButtonUI text={"Supprimer l'utilisateur"} className={"button-secondary"}/>
                     </div>
                 </form>
--- src/services/supabase/supabaseDatabase.js
+++ src/services/supabase/supabaseDatabase.js
@@ -4,3 +4,8 @@ export async function supabaseGetUsers() {
     let { data: users, error } = await supabase.from('users').select('*');
     return users;
 }
+
+export async function supabaseGetUserById(userId) {
+    let { data: user, error } = await supabase.from('users').select("*").eq('id', userId);
+    return user;
+}
