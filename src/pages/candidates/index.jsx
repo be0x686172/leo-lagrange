8da775bf2c72eef95cc847eb1704823b1c1e34c0
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-10-30 19:25:20 +0100

    save

--- src/pages/candidates/index.jsx
+++ src/pages/candidates/index.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TableContainerFeature from '../../components/features/table-container/container';
 import candidatesData from './data.json';
+import BadgeUI from '../../components/ui/badge';
 
 const CandidatesPage = () => {
 
-    const [users, setUsers] = useState([]);
+    const [candidates, setCandidates] = useState([]);
+
+    useEffect(() => {
+        const transformedData = candidatesData.map(candidat => ({
+            ...candidat,
+            job: <BadgeUI text={candidat.job} className={"badge-secondary"} />
+        }));
+
+        setCandidates(transformedData);
+    }, [candidatesData]);
 
     return (
         <div className="page candidates-page">
-            <TableContainerFeature version={"candidates"} columns={["Date de candidature", "Nom", "Prénom", "Poste"]} data={candidatesData} lengthData={Object.keys(candidatesData).length}/>
+            <TableContainerFeature version={"candidates"} columns={["Date de candidature", "Nom", "Prénom", "Poste"]} data={candidates} lengthData={Object.keys(candidatesData).length}/>
         </div>
     );
 };
