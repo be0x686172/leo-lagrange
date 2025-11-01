852f274e7c941b5427ea1472b7f3e38cfe69f836
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-11-01 07:27:31 +0100

    save

--- src/components/features/table-container/view-informations-candidates/index.jsx
+++ src/components/features/table-container/view-informations-candidates/index.jsx
@@ -16,16 +16,62 @@ const ViewInformationsCandidatesTableContainerFeature = ({ candidatId }) => {
         <div className={`view-informations-candidates-table-container-feature ${candidatId == null ? 'view-informations-candidates-table-container-feature-center': ''}`}>
             {candidatId == null ? <p className='no-candidat-id'>Sélectionnez un candidat pour consulter sa fiche</p> : (
                 <>
-                    <div>
+                    <div className='header'>
                         <h1><span>{candidat.name}</span> {candidat.firstname}</h1>
                         <p>{candidat.job}</p>
                     </div>
-                    <div>
+                    <div className='main'>
                         <div>
                             <p>Horaires</p>
-                            {candidat.working_hours ? candidat.working_hours.map((hours) => {
-                                {console.log(hours)}
-                            }) : ''}
+                            <div>
+                                {candidat.working_hours ? candidat.working_hours.map((hours, i) => (
+                                    <BadgeUI text={hours} key={i} className={"badge-default"} />
+                                )) : ''}
+                            </div>
+                        </div>
+                        <div>
+                            <p>Arrondissements</p>
+                            <div>
+                                {candidat.districts ? candidat.districts.map((district, i) => (
+                                    <BadgeUI text={district + 'er'} key={i} className={"badge-default"} />
+                                )) : ''}
+                            </div>
+                        </div>
+                        <div>
+                            <p>Date de candidature :</p>
+                            <div>
+                                <p>{candidat.application_date}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p>Téléphone :</p>
+                            <div>
+                                <p>{candidat.phone_number}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p>E-mail :</p>
+                            <div>
+                                <p>{candidat.email}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p>Diplôme :</p>
+                            <div>
+                                <p>{candidat.diploma}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p>Expériences avec les enfants :</p>
+                            <div>
+                                {candidat.children_experience ? <p>Oui</p> : <p>Non</p>}
+                            </div>
+                        </div>
+                        <div className='commentary'>
+                            <p>Commentaire :</p>
+                            <div>
+                                <p>{candidat.commentary}</p>
+                            </div>
                         </div>
                     </div>
                 </>
