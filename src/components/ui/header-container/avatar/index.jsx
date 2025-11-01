5d389283f59c545db9bde9eed62687717aac3d3c
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-11-01 11:17:58 +0100

    save

--- src/components/ui/header-container/avatar/index.jsx
+++ src/components/ui/header-container/avatar/index.jsx
@@ -1,12 +1,30 @@
 import './style.scss';
+import { supabaseGetSession } from '../../../../services/supabase/supabaseAuthentication';
+import { useEffect, useState } from 'react';
+import { supabaseGetUserById } from '../../../../services/supabase/supabaseUsersDatabase';
 
-const index = ({ openHeader }) => {
+const AvatarUI = ({ openHeader }) => {
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        supabaseGetSession().then((success) => {
+            const userId = success.session.user.id;
+
+            supabaseGetUserById(userId).then((data) => {
+                if (data) {
+                    setUser({...data[0]})
+                }
+            });
+        })
+    }, [user])
+
     return (
         <div className='avatar-ui'>
             <div><p><span>M</span><span>B</span></p></div>
-            {openHeader ? <p><span>Mouad</span> <br /> <span>Behar</span></p> : ''}
+            {openHeader ? <p><span>{user.firstname}</span> <br /> <span>{user.name}</span></p> : ''}
         </div>
     );
 };
 
-export default index;
+export default AvatarUI;
